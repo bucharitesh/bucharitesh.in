f8f22c56e58b31608d40951dc381fa449fa98f07
Ritesh Bucha <bucharitesh@gmail.com>
2024-09-15 03:06:29 +0530

    chore: add blog on useaction state

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getAPI() {
+  if(process.env.NODE_ENV === 'development') {
+    return 'http://localhost:8888'
+  }
+
+  return "https://bucharitesh.in"
+}
