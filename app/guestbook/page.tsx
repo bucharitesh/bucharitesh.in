951f369e7b4fb8fcc689f0677ce4a466d0fa93ee
Ritesh Buchas <bucharitesh@gmail.com>
2025-09-12 17:16:19 +0530

    chore: ~ minor fixes

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -11,7 +11,6 @@ import WriteNoteCTA from '@/features/guestbook/write';
 import { createOgImage } from '@/lib/createOgImage';
 import { createMetadata } from '@/lib/seo/metadata';
 import { cn } from '@/lib/utils';
-import { Provider } from 'jotai';
 import type { Metadata } from 'next/types';
 import styles from './notes.module.css';
 
@@ -34,69 +33,54 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export const dynamic = 'force-dynamic';
 
-// export default async function GuestbookPage() {
-//   return (
-//     <div className="h-screen w-screen" style={{
-//       "backgroundColor": "#06c",
-//       "backgroundImage": "linear-gradient(rgba(255,255,255,0.2) 2px, transparent 2px), linear-gradient(90deg, rgba(255,255,255,0.2) 2px, transparent 1px), linear-gradient(rgba(255,255,255,0.1) 1px, transparent 1px), linear-gradient(90deg, rgba(255,255,255,.1) 1px, transparent 1px)",
-//       "backgroundSize": "100px 100px, 100px 100px, 20px 20px, 20px 20px",
-//       "backgroundPosition": "-2px -2px, -2px -2px, -1px -1px, -1px -1px",
-//     }}>
-//       hello
-//     </div>
-//   );
-// }
-
 export default function GuestbookPage() {
   return (
-    <Provider>
-      <ScrollArea useScrollAreaId>
-        <FloatingHeader title="Guestbook" />
+    <ScrollArea useScrollAreaId>
+      <FloatingHeader title="Guestbook" />
+      <div
+        className={cn('h-screen w-screen bg-[#0565c6]')}
+        style={{
+          backgroundSize: '100px 100px, 100px 100px, 20px 20px, 20px 20px',
+          backgroundPosition: '-2px -2px, -2px -2px, -1px -1px, -1px -1px',
+        }}
+      >
         <div
-          className={cn('h-screen w-screen bg-[#0565c6]')}
-          style={{
-            backgroundSize: '100px 100px, 100px 100px, 20px 20px, 20px 20px',
-            backgroundPosition: '-2px -2px, -2px -2px, -1px -1px, -1px -1px',
-          }}
+          id="mat-container"
+          className={cn('relative h-full w-full overflow-hidden')}
         >
-          <div
-            id="mat-container"
-            className={cn('relative h-full w-full overflow-hidden')}
-          >
-            <div className="z-10">
-              <div id="mat-grid" className={styles.matGrid}>
-                <div
-                  className={
-                    'absolute inset-0 bg-position-center bg-size-[10vmin_10vmin]'
-                  }
-                  style={{
-                    backgroundImage:
-                      'linear-gradient(45deg,transparent 49.5%,rgba(255, 255, 255, 0.2) 49.5%,rgba(255, 255, 255, 0.2) 50.5%,transparent 50.5%),linear-gradient(-45deg,transparent 49.5%,rgba(255, 255, 255, 0.2) 49.5%,rgba(255, 255, 255, 0.2) 50.5%,transparent 50.5%)',
-                  }}
-                />
-              </div>
+          <div className="z-10">
+            <div id="mat-grid" className={styles.matGrid}>
+              <div
+                className={
+                  'absolute inset-0 bg-position-center bg-size-[10vmin_10vmin]'
+                }
+                style={{
+                  backgroundImage:
+                    'linear-gradient(45deg,transparent 49.5%,rgba(255, 255, 255, 0.2) 49.5%,rgba(255, 255, 255, 0.2) 50.5%,transparent 50.5%),linear-gradient(-45deg,transparent 49.5%,rgba(255, 255, 255, 0.2) 49.5%,rgba(255, 255, 255, 0.2) 50.5%,transparent 50.5%)',
+                }}
+              />
             </div>
-            <main className="relative z-20 h-full w-full overflow-hidden">
-              <GuestbookEntries />
-              <Polaroid
-                src="https://cdn.bucharitesh.in/guestbook/photo_1.jpeg"
-                alt="kedarkantha"
-              />
-              <Polaroid
-                src="https://cdn.bucharitesh.in/guestbook/photo_2.jpg"
-                alt="graduation"
-              />
-              <Sticker>
-                <VercelLogo />
-              </Sticker>
-              <Sticker>
-                <NextWordmark />
-              </Sticker>
-              <WriteNoteCTA />
-            </main>
           </div>
+          <main className="relative z-20 h-full w-full overflow-hidden">
+            <GuestbookEntries />
+            <Polaroid
+              src="https://cdn.bucharitesh.in/guestbook/photo_1.jpeg"
+              alt="kedarkantha"
+            />
+            <Polaroid
+              src="https://cdn.bucharitesh.in/guestbook/photo_2.jpg"
+              alt="graduation"
+            />
+            <Sticker>
+              <VercelLogo />
+            </Sticker>
+            <Sticker>
+              <NextWordmark />
+            </Sticker>
+            <WriteNoteCTA />
+          </main>
         </div>
-      </ScrollArea>
-    </Provider>
+      </div>
+    </ScrollArea>
   );
 }
