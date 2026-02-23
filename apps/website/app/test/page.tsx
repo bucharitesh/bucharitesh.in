602f4d7cd47811e560ebb7dacdef595557268a33
Ritesh Buchas <bucharitesh@gmail.com>
2026-02-24 01:14:19 +0530

    chore: add metallic-button

---
+++ apps/website/app/test/page.tsx
@@ -0,0 +1,12 @@
+import MetallicButtonDemo from '@/registry/example/metallic-button-demo';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <MetallicButtonDemo />
+    </div>
+  );
+};
+
+export default Page;
